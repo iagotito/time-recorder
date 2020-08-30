e51076b830c3be40b859e6b7a04a4e4e8c1b02d8
Iago Tito Oliveira <iago.oliveira@ccc.ufcg.edu.br>
2020-08-30 17:20:47 -0300

    add login and logout functions

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
-let $logTxt = document.querySelector("#logTxt");
-let $logBtn = document.querySelector("#logBtn");
-let $testConnectionBtn = document.querySelector("#testConnectionBtn");
+let $logTxt = document.querySelector("#log_txt");
+let $logBtn = document.querySelector("#log_btn");
+let $testConnectionBtn = document.querySelector("#test_connection_btn");
+let authorizeButton = document.querySelector('#authorize_button');
+let signoutButton = document.querySelector('#signout_button');
+
+authorizeButton.addEventListener("click", () => {
+  handleAuthClick();
+});
+
+signoutButton.addEventListener("click", () => {
+  handleSignoutClick();
+});
+
+$logBtn.addEventListener("click", () => {
+  let logTxt = $logTxt.value;
+  $logTxt.value = "";
+
+  let log_time = get_time();
+
+  //TODO: exclude this logs and save informations in google sheets
+  console.log(logTxt);
+  console.log(log_time);
+});
+
+$testConnectionBtn.addEventListener("click", () => {
+  start();
+});
 
 init();
 
 function init () {
-    $logBtn.addEventListener("click", () => {
-    logTxt = $logTxt.value;
-    $logTxt.value = "";
-
-    let log_time = get_time();
-
-    //TODO: exclude this logs and save informations in google sheets
-    console.log(logTxt);
-    console.log(log_time);
-    });
-    
-    $testConnectionBtn.addEventListener("click", () => {
-      start();
-    });
+  console.log("init main");
 }
 
 function get_time () {
--- sheets.js
+++ sheets.js
@@ -6,15 +6,37 @@ async function init () {
         clientId: CLIENT_ID,
         scope: SCOPES
     });
-    await gapi.client.load('sheets', 'v4');
+    await gapi.client.load("sheets", "v4");
+    await gapi.load("auth2", async () => {
+        await gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        let isSignedIn = await gapi.auth2.getAuthInstance().isSignedIn.get()
+        updateSigninStatus(isSignedIn);
+    });
 }
 
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+async function handleAuthClick(event) {
+    await gapi.auth2.getAuthInstance().signIn();
+}
+
+async function handleSignoutClick(event) {
+    await gapi.auth2.getAuthInstance().signOut();
+}
 
 async function start() {
     try {
         let apiRequest = await gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: '1MhkvVs4OfkNUK1WL2D964yeXQnIFzVgjOw0WaudhFbw',
-            range: 'Data!A:B',
+            spreadsheetId: "1MhkvVs4OfkNUK1WL2D964yeXQnIFzVgjOw0WaudhFbw",
+            range: "Data!A:B",
         });
         let res = JSON.parse(apiRequest.body);
         console.log(res);
