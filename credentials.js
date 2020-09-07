a569ae915937571b65f5ef14dba90c2e168679fc
Iago Tito Oliveira <iago.oliveira@ccc.ufcg.edu.br>
2020-09-07 16:29:51 -0300

    adds credentials file
    The credentials file have the credentials to allow acess of this API
    to the user's spreadsheet.

---
+++ credentials.js
@@ -0,0 +1,7 @@
+const CLIENT_ID = "849572965269-cfgkctigui3hm2thfm31a9v8vqsa9672.apps.googleusercontent.com";
+const API_KEY = "AIzaSyDb4gID4_RPWqFmjpYFW-y0UkYdgj41KDw";
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+// To use your own spreadsheet you have to add it's ID here. The spreadsheet's ID
+// is the text after the "/d/" in it's URL.
+// Remember to sign in with google to an account that have access to this spreadsheet.
+const SPREADSHEET_ID = "YOUR SPREADSHEET'S ID HERE";
